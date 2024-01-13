b72ae07906f1be7fbdb3cc6ccbcd2b8ec4745f20
yuki hasegawa <nphsgw6211yk@gmail.com>
2024-01-13 09:21:22 +0900

    update

--- 4_classification/4_2_cnn/eval.py
+++ 4_classification/4_2_cnn/eval.py
@@ -5,25 +5,27 @@ from torch import nn
 from torch.utils.data import Dataset
 
 
-'''
+"""
 data_loader: 評価に使うデータを読み込むデータローダ
 model      : 評価対象のモデル
 loss_func  : 目的関数
-'''
-def evaluate(data_loader: Dataset, model: nn.Module,
-             loss_func: Callable):
+"""
+
+
+def evaluate(data_loader: Dataset, model: nn.Module, loss_func: Callable):
     model.eval()
 
     losses = []
     preds = []
     for x, y in data_loader:
+        # 評価時は勾配計算をしない
         with torch.no_grad():
             x = x.to(model.get_device())
             y = y.to(model.get_device())
 
             y_pred = model(x)
 
-            losses.append(loss_func(y_pred, y, reduction='none'))
+            losses.append(loss_func(y_pred, y, reduction="none"))
             preds.append(y_pred.argmax(dim=1) == y)
 
     loss = torch.cat(losses).mean()
